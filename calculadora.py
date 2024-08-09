83dc9842cbdb495e54643c47735c8c8a167437ad
Marcos <marcosranier34@gmail.com>
2024-08-09 16:13:37 -0300

    Modifica layout dos botões

--- calculadora.py
+++ calculadora.py
@@ -48,14 +48,30 @@ tk.Entry(root, textvariable=entrada1).grid(row=0, column=1)
 
 tk.Label(root, text="Segundo número: ").grid(row=1, column=0)
 tk.Entry(root, textvariable=entrada2).grid(row=1, column=1)
+button_style= {
+ #Fonte
+    'font': ('Arial', 16),
+#Cor do fundo do botão
+    'bg': '#D92525',
+#Cor do texto
+    'fg': '#ffffff',
+#Tipo de borda do botão
+    'relief': 'raised',
+#Tamanho da borda
+    'bd': 3,
+#Largura do botão
+    'width': 1,
+#Altura do botão
+    'height': 1
+}
 
-tk.Button(root, text="+", command=adicionar).grid(row=2, column=0)
-tk.Button(root, text="-", command=subtrair).grid(row=2, column=1)
-tk.Button(root, text="*", command=multiplicar).grid(row=2, column=2)
-tk.Button(root, text="/", command=dividir).grid(row=2, column=3)
+tk.Button(root, text="+", command=adicionar, **button_style).grid(row=0, column=2)
+tk.Button(root, text="-", command=subtrair, **button_style).grid(row=0, column=3)
+tk.Button(root, text="*", command=multiplicar, **button_style).grid(row=1, column=2)
+tk.Button(root, text="/", command=dividir, **button_style).grid(row=1, column=3)
 
 tk.Label(root, text="Resultado: ").grid(row=3, column=0)
-tk.Entry(root, textvariable=resultado, state='readonly').grid(row=3, column=1)
+tk.Entry(root, textvariable=resultado, state='readonly').grid(row=2, column=1)
 
 #iniciar a aplicação
 root.mainloop()
